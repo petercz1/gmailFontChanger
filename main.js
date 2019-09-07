5f68654e2f0f548ed992330b22897565b0fcceae
petercz1 <email.peter.carroll@gmail.com>
2019-09-07 15:00:19 +0800

    The fashion wears out more apparel than the man. 		-- William Shakespeare, "Much Ado About Nothing"

---
+++ main.js
@@ -0,0 +1,37 @@
+// start things off
+// I set the check interval at 1000 milliseconds
+setInterval(() => removeStyles(document.body), 1000);
+
+// recursion - this is called if the
+// element has child elements
+removeStyles(document.body);
+
+function removeStyles(el) {
+  // uncomment this if you want to remove all styles from element node
+  // el.removeAttribute('style');
+
+  // I'm just removing the bloody annoying 'small' fontsize
+  // and setting my own. And changing font and line spacing
+  // while I'm at it.
+  el.style.fontSize = "medium";
+  el.style.fontFamily = "Ariel";
+  el.style.lineHeight = "1.5";
+
+  // other ideas...
+  // el.style.fontStyle = "normal|italic|oblique|initial|inherit";
+  // el.style.fontVariant = "small-caps";
+
+
+  if (el.childNodes.length > 0) {
+    for (var child in el.childNodes) {
+      // FYI...
+      // nodeType == 1 - element
+      // nodeType == 2 - attribute
+      // nodeType == 3 - text
+      // nodeType == 8 - comment
+      if (el.childNodes[child].nodeType == 1)
+        // recursion here
+        removeStyles(el.childNodes[child]);
+    }
+  }
+}
